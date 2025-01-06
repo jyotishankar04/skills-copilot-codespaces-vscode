d642b65fb32c5b3a39cf10486d404166a70c2610
jyotishankar04 <patrajyotishankar@gmail.com>
2025-01-06 22:39:23 +0530

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,18 @@
 // create a web server
-// create a route for /comments
-// read the comments.json file and send the contents as a response
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+// create a route to handle comments
+app.get("/comments", function (req, res) {
+  res.send("This is the comments page");
+});
+
+// create a route to handle comments
+app.post("/comments", function (req, res) {
+  res.send("You have sent a POST request");
+});
+
+// start the server
+app.listen(3000);
+console.log("Server is listening on port 3000");
